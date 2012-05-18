f6e477e39d38a92d849a7183fe00da9720fd63b1
gelefisk <oekj@online.no>
2012-05-18 23:26:22 +0200

    Replaced the django-project with a new, empty ruby on rails project. #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviestack::Application.config.secret_token = 'dc195b657aa4305f952c0918a43ce2c9a9c5c6ea591ab2c4b5a4c6c43f2c4ed4c7c38340354652bd8597f685f7094c8ccc9adc72b7b6b02c2a398f7614d2776e'
